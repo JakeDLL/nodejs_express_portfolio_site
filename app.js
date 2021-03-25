28910968355c3ad35a88f7601653029163cd58bd
Jacob D <35861176+JakeDLL@users.noreply.github.com>
2021-03-25 19:19:26 -0400

    Add error handlers to app.js and add error template to views folder

--- app.js
+++ app.js
@@ -15,12 +15,37 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.get('/project/:id', (req, res) => {
+app.get('/project', (req, res) => {
+    const randomProject = Math.floor(Math.random() * projects.length);
+    res.redirect(`/project/${randomProject}`);
+});
+
+app.get('/project/:id', (req, res, next) => {
     const { id } = req.params;
+    if (isNaN(id)) {
+        const err = new Error('Bad Request: No such project');
+        err.status = 400;
+        return next(err);
+    }
     const project = projects[id];
 
     res.render('project', { project });
 });
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    err.message = `Page does not exist`
+    next(err);
+})
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status || 500);
+    if (!res.locals.error.message) res.locals.error.message = `There is a problem with the server`;
+    console.log(`${err.message} - (${err.status})`);
+    res.render('error');
+})
 
 app.listen(3000, () => {
     console.log('The app is running on localhost:3000');
